a9ce16ddef692a8548cadf85ae64b959f6f512d0
mlkiung <meredith.kiung@gmail.com>
2017-06-21 12:34:13 -0400

    updated table.jsx

--- app/components/Table.jsx
+++ app/components/Table.jsx
@@ -20,7 +20,7 @@ const Table = (props) => (
       {
         props.products && props.products.map((product, i) => (
           <tr key={i}>
-            <td>{product.thumbnailImage}</td>
+            <td><img src={product.thumbnailImage} /></td>
             <td>{product.name}</td>
             <td><a href={product.productUrl} target="_blank"><span className="glyphicon glyphicon-new-window" aria-hidden="true" aria-label="Open product in a new window"></span></a></td>
             <td>{product.brandName}</td>
@@ -29,7 +29,7 @@ const Table = (props) => (
             <td>{`${product.msrp ? `$${product.msrp}` : '(none)'}`}</td>
             <td>{product.customerRating}</td>
             <td>{`(${product.numReviews})`}</td>
-            <td></td>
+            <td><button type="submit" className="btn btn-default" aria-label="Left Align" onClick={handleClick} name={item.itemId}><span className="glyphicon glyphicon-remove" aria-hidden="true"></span></button></td>
           </tr>
         ))
       }
