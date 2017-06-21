1b28abcc0e53d535ca934c741fb37c28588cc6f2
mlkiung <meredith.kiung@gmail.com>
2017-06-21 12:48:00 -0400

    dumbed-down productsList

--- app/components/ProductsList.jsx
+++ app/components/ProductsList.jsx
@@ -1,42 +1,13 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React from 'react'
 
 import Table from './Table'
 import TableControlPanel from './TableControlPanel'
 
-class ProductsList extends Component {
-  constructor() {
-    super()
+const ProductsList = () => (
+  <div id="products-container">
+    <TableControlPanel />
+    <Table />
+  </div>
+)
 
-    this.state = {
-      products: [],
-    }
-  }
-
-  // componentDidMount() {
-  //   this.setState({products: this.props.products})
-  // }
-
-  // componentWillReceiveProps(nextProps) {
-  //   console.log('nextProps in ProductsList.jsx', nextProps)
-  //   if (nextProps.products !== this.props.products) {
-  //     this.setState({ products: nextProps.products })
-  //   }
-  // }
-
-  render() {
-    const products = this.state.products
-    console.log('this.state in products list', this.state)
-
-    return (
-      <div id="products-container">
-        <TableControlPanel />
-        <Table />
-      </div>
-    )
-  }
-}
-
-const mstp = (state) => ({products: state.items})
-
-export default connect(mstp)(ProductsList)
+export default ProductsList
