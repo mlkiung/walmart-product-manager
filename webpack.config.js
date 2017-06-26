e34a1c8c0f1e3257649573f2e6ab92cce01c231b
mlkiung <meredith.kiung@gmail.com>
2017-06-26 11:30:09 -0400

    housekeeping

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,11 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
-// const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
   template: path.join(__dirname, '/public/index.html'),
   filename: 'index.html',
   inject: 'body'
 })
-
-// const extractCSS = new ExtractTextPlugin({filename: 'style.css'})
 
 const config = {
   entry: './app/index.js',
@@ -20,10 +17,7 @@ const config = {
     rules: [
       {
         test: /\.css$/,
-        // use: ExtractTextPlugin.extract({
-        // fallback: 'style-loader',
         use: ['style-loader', 'css-loader']
-        // })
       },
       {
         test: /\.js$/,
@@ -39,7 +33,6 @@ const config = {
   },
   plugins: [
     HTMLWebpackPluginConfig,
-    // extractCSS
   ],
   resolve: {
     extensions: ['.js', '.jsx', '.css']
