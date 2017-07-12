f3f2a7f7130b1ae2302a62f9d9920fee1d29e209
mlkiung <meredith.kiung@gmail.com>
2017-07-12 16:06:55 -0400

    local server working

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const {resolve} = require('path')
-// const chalk = require('chalk')
-const pkg = require('./package.json')
-const debug = require('debug')(`${pkg.name}:boot`)
+// const {resolve} = require('path')
+// // const chalk = require('chalk')
+// const pkg = require('./package.json')
+// const debug = require('debug')(`${pkg.name}:boot`)
 
-const env = Object.create(process.env)
-  , secretsFile = resolve(env.HOME, `.${pkg.name}.env`)
-try {
-  Object.assign(env, require(secretsFile))
-} catch (error) {
-  debug('%s: %s', secretsFile, error.message)
-  debug('%s: env file not found or invalid, moving on', secretsFile)
-}
+// const env = Object.create(process.env)
+//   , secretsFile = resolve(env.HOME, `.${pkg.name}.env`)
+// try {
+//   Object.assign(env, require(secretsFile))
+// } catch (error) {
+//   debug('%s: %s', secretsFile, error.message)
+//   debug('%s: env file not found or invalid, moving on', secretsFile)
+// }
 
-const PORT = process.env.PORT || 1337
+// const PORT = process.env.PORT || 8080
 
-module.exports = {
-  get name() {
-    return pkg.name
-  },
-  get isTesting() {
-    return !!global.it
-  },
-  get isProduction() {
-    return process.env.NODE_ENV === 'production'
-  },
-  get baseUrl() {
-    return env.BASE_URL || `http://localhost:${PORT}`
-  },
-  get port() {
-    return env.PORT || 1337
-  },
-  package: pkg,
-  env
-}
+// module.exports = {
+//   get name() {
+//     return pkg.name
+//   },
+//   get isTesting() {
+//     return !!global.it
+//   },
+//   get isProduction() {
+//     return process.env.NODE_ENV === 'production'
+//   },
+//   get baseUrl() {
+//     return env.BASE_URL || `http://localhost:${PORT}`
+//   },
+//   get port() {
+//     return env.PORT || 1337
+//   },
+//   package: pkg,
+//   env
+// }
--- server/start.js
+++ server/start.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const { resolve } = require('path')
 
-const pkg = require('../index.js')
+// const pkg = require('../index.js')
 
 const app = express()
 
+app.use(require('volleyball'))
+
 module.exports = app
-  .use(express.static(resolve(__dirname, '..', 'public')))
+  .use(express.static(resolve(__dirname, '..', 'build')))
+  .get('/*', (_, res) => res.sendFile(resolve(__dirname, '..', 'build', 'index.html')))
   .listen(process.env.PORT || 8080)
