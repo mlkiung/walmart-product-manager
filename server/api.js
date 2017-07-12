66cb5c8cf8b9816efe64e46c7d55ceb059e49e48
mlkiung <meredith.kiung@gmail.com>
2017-07-12 15:06:53 -0400

    deploy

--- server/api.js
+++ server/api.js
@@ -3,4 +3,4 @@
 const api = module.exports = require('express').Router()
 
 // No routes matched? 404.
-api.use((req, res) => res.status(404).end())
+api.get('/')
--- server/start.js
+++ server/start.js
@@ -7,17 +7,4 @@ const app = express()
 
 module.exports = app
   .use(express.static(resolve(__dirname, '..', 'public')))
-  .get('/*', (_, res) => res.sendFile(resolve(__dirname, '..', 'public', 'index.html')))
-  .use('/api', require('./api'))
-
-const server = app.listen(process.env.PORT || 1337, () => {
-  console.log(`--- Started HTTP Server for ${pkg.name} ---`)
-  const { address, port } = server.address()
-  const host = address === '::'
-    ? 'localhost'
-    : address
-  const urlSafeHost = host.includes(':')
-    ? `[${host}]`
-    : host
-  console.log(`Listening on http://${urlSafeHost}:${port}`)
-})
+  .listen(process.env.PORT || 8080)
