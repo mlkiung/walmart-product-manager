47aeceb07de3b29a1e89fc5cfd754cd68f2385e0
mlkiung <meredith.kiung@gmail.com>
2017-07-12 14:19:37 -0400

    creating dev and prod

---
+++ server/start.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const { resolve } = require('path')
+
+const pkg = require('../index.js')
+
+const app = express()
+
+module.exports = app
+  .use(express.static(resolve(__dirname, '..', 'public')))
+  .get('/*', (_, res) => res.sendFile(resolve(__dirname, '..', 'public', 'index.html')))
+
+const server = app.listen(process.env.PORT || 1337, () => {
+  console.log(`--- Started HTTP Server for ${pkg.name} ---`)
+  const { address, port } = server.address()
+  const host = address === '::'
+    ? 'localhost'
+    : address
+  const urlSafeHost = host.includes(':')
+    ? `[${host}]`
+    : host
+  console.log(`Listening on http://${urlSafeHost}:${port}`)
+})
